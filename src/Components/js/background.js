6b2a391ed00034252c3dfdf772e6f7523792bcc9
Web403 <sonawaneomkar2306@gmail.com>
2023-07-11 11:57:47 +0530

    Ui Improved

--- src/Components/js/background.js
+++ src/Components/js/background.js
@@ -2,18 +2,23 @@ import React from 'react';
 import "../css/background.css";
 import "./imgslider";
 import imgslider from './imgslider';
+import {AiOutlineArrowRight} from 'react-icons/ai'
 
 const background = () => {
     return (
         <div className='container'>
             <div className='main'>
                 <div className='upper-side'>
-                    <h1 id='upper-heading'>CHOOSE<br></br> CHARACTER</h1>
+                    <h1 id='upper-heading'>MARVEL<br></br> CHARACTER</h1>
                 </div>
                 <p>Avengers</p>
             </div>
             <div className='bottom-side'>
                 {imgslider()}
+            </div>
+            <div id='footer'>
+            <h3 id='footer-text'>SLIDE RIGHT</h3>
+            <AiOutlineArrowRight id='icon'/>
             </div>
         </div>
     );
